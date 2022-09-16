91ba142d99e9300ce051c5a238d4da6d707561d2
lucashemi <lucasxberger@gmail.com>
2022-09-16 14:31:41 -0300

    pamixer

--- config.h
+++ config.h
@@ -8,10 +8,10 @@ static const char *brup[]    = { "brightnessctl", "set", "5%+", NULL };
 static const char *brdown[]  = { "brightnessctl", "set", "5%-", NULL };
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -95,9 +95,9 @@ static const char *xsecurelockcmd[]  = { "xsecurelock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_F1,     spawn, SHCMD("pactl set-sink-mute 0 toggle; kill -44 $(pidof dwmblocks)")  },
-	{ MODKEY,                       XK_F2,     spawn, SHCMD("pactl set-sink-mute 0 false ; pactl set-sink-volume 0 -5%; kill -44 $(pidof dwmblocks)")  },
-	{ MODKEY,                       XK_F3,     spawn, SHCMD("pactl set-sink-mute 0 false ; pactl set-sink-volume 0 +5%; kill -44 $(pidof dwmblocks)")  },
+	{ MODKEY,                       XK_F1,     spawn, SHCMD("pamixer -t ; kill -44 $(pidof dwmblocks)")  },
+	{ MODKEY,                       XK_F2,     spawn, SHCMD("pamixer -u ; pamixer -d 5; kill -44 $(pidof dwmblocks)")  },
+	{ MODKEY,                       XK_F3,     spawn, SHCMD("pamixer -u ; pamixer -i 5; kill -44 $(pidof dwmblocks)")  },
 	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 	{ MODKEY,			XK_F9,     spawn, SHCMD("setxkbmap -layout us; kill -RTMIN+30 $(pidof dwmblocks)") },
 	{ MODKEY,			XK_F10,    spawn, SHCMD("setxkbmap -layout br; kill -RTMIN+30 $(pidof dwmblocks)") },	
